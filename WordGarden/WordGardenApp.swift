3d76b3c8d3b50cb0d5a27f39c95ae5f0405c103c
Tad <tadclifton@icloud.com>
2024-09-23 13:46:20 -0400

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Tad Clifton on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
